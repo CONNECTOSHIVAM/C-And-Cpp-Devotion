5451d5bb1632f631d8729744042d92f1013eab02
connectoshivam <connecttoshivam1@gmail.com>
2026-01-24 11:08:18 +0530

    find the area of reftraingle

---
+++ C-Basic/Ques6.c
@@ -0,0 +1,13 @@
+// Q6. calulate the area of reftraingle.
+
+#include<stdio.h>
+int main(){
+
+    int length, breadth;
+    printf("Entet the Length of reftraingle: ");
+    scanf("%d",&length);
+    printf("Enter the Breadth of reftraingle: ");
+    scanf("%d",&breadth);
+    printf("Area of reftraingle: %d",length*breadth);
+    return 0;
+}
