e34f38f1f4a0209739a929c91d2465f9440454a7
connectoshivam <connecttoshivam1@gmail.com>
2026-01-24 13:45:07 +0530

    circumference of the circle

---
+++ C-Basic/Ques9.c
@@ -0,0 +1,11 @@
+// Q. calculate the circumference of circle.
+
+#include<stdio.h>
+int main(){
+    int radius;
+    printf("Enter the radius the circle of radius:");
+    scanf("%d",&radius);
+    float circumfarence = 2*3.14*radius;
+    printf("circumfarence of the Circle: %.2f",circumference);
+    return 0;
+}
