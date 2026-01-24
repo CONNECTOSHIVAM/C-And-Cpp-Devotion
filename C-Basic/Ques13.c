31abf54091b1bf644f4c5e5246ae21d9b0c2fc78
connectoshivam <connecttoshivam1@gmail.com>
2026-01-24 14:43:43 +0530

    print the ASCII value

---
+++ C-Basic/Ques13.c
@@ -0,0 +1,11 @@
+// Q. Write a program to input a character from the user and print its ASCII CODE.
+
+#include<stdio.h>
+int main(){
+
+    char a;
+    printf("Enter the one character: ");
+    scanf("%c",&a);
+    printf("value of enter %c character is : %d",a,a);
+    return 0;
+}
