ed7219b7fae5a6b8a147a835a1bbe2ae7700fb5a
connectoshivam <connecttoshivam1@gmail.com>
2026-01-24 13:46:02 +0530

    circumference of the circle

--- C-Basic/Ques9.c
+++ C-Basic/Ques9.c
@@ -5,7 +5,7 @@ int main(){
     int radius;
     printf("Enter the radius the circle of radius:");
     scanf("%d",&radius);
-    float circumfarence = 2*3.14*radius;
+    float circumference = 2*3.14*radius;
     printf("circumfarence of the Circle: %.2f",circumference);
     return 0;
 }
