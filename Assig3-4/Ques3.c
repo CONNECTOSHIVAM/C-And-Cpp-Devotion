59cd2ab27275005740c5a3e07b00496b2369230a
connectoshivam <connecttoshivam1@gmail.com>
2026-01-27 21:04:33 +0530

    char analogy

---
+++ Assig3-4/Ques3.c
@@ -0,0 +1,13 @@
+// Q. write a program with one char type variable . Assign 'A' in the variable now 
+// change the value of variable from 'A' to 'B' using increment operator.
+
+#include<stdio.h>
+int main(){
+
+    char one = 'A';
+    one++;
+    printf("%c",one);
+
+
+    return 0;
+}
