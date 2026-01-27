e13a71c316d406e615b7a29c314b86416436409c
connectoshivam <connecttoshivam1@gmail.com>
2026-01-27 20:32:20 +0530

    basic

---
+++ Assig3-4/Ques1.c
@@ -0,0 +1,11 @@
+// Q.write a program to calculate the size of character constants.
+
+#include<stdio.h>
+int main(){
+    printf("radhe radhe");
+
+    int a;
+    a = sizeof('s');
+    printf("\n size of character: %d",a);
+    return 0;
+}
