ab79ed10cad262d5552cd0ca157525d80ff445a7
connectoshivam <connecttoshivam1@gmail.com>
2026-01-28 09:01:33 +0530

    basics

---
+++ Assig3-4/Ques16.c
@@ -0,0 +1,13 @@
+// Q.ruppee to dollar conversion.
+
+#include<stdio.h>
+int main()
+{
+    int inr = 574;
+
+    float dollar = inr/84.23;
+
+    printf(" ₹%d INR in Dollar: $%.2f",inr, dollar);
+
+    return 0;
+}
