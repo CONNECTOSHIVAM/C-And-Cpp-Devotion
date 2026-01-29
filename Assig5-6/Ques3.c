8c463905a97b2279aa0b3e20b7938c377cfbae17
connectoshivam <connecttoshivam1@gmail.com>
2026-01-29 09:13:09 +0530

    basics

---
+++ Assig5-6/Ques3.c
@@ -0,0 +1,15 @@
+// Q. check the entered number is even and odd.
+
+#include<stdio.h>
+int main(){
+
+    int n = 10;
+    
+    if(n%2==0){
+        printf("%d number is even",n);
+    }else{
+        printf("%d number is even",n);
+    }
+
+    return 0;
+}
