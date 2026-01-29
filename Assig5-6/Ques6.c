b75e412f3d5590692cf1978ff1fc09fe3422dbdd
connectoshivam <connecttoshivam1@gmail.com>
2026-01-29 11:06:25 +0530

    basics

--- Assig5-6/Ques6.c
+++ Assig5-6/Ques6.c
@@ -3,17 +3,10 @@
 #include<stdio.h>
 int main(){
 
-    int a = 456;
-    int count;
+    int a = 4506;
+  
 
-    while (a>0)
-    {
-        int z = a%10;
-        count++;
-        a = a/10;
-    }
-
-    if(count==3)
+    if(a>=100 && a <1000)
     {
         printf("Enter digits length is 3");
     }else{
