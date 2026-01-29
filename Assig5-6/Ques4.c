aae18800b5ce6de0122ecb0a6dc60acf1539ca07
connectoshivam <connecttoshivam1@gmail.com>
2026-01-29 09:38:20 +0530

    basics

---
+++ Assig5-6/Ques4.c
@@ -0,0 +1,14 @@
+// Q.
+
+#include<stdio.h>
+int main(){
+
+    int num = 7;
+
+    if(num/2*2==num)
+    {
+        printf("%d number is even.",num);
+    }else{
+        printf("%d number is odd.",num);
+    }
+}
