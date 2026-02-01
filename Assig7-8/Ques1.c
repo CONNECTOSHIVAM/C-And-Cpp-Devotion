eb2739aaf3f4a4ecf7e13947b74b50120b4276ca
connectoshivam <connecttoshivam1@gmail.com>
2026-02-01 10:09:21 +0530

    basics

---
+++ Assig7-8/Ques1.c
@@ -0,0 +1,13 @@
+// Q.write a program to print the first 10 natural numbers.
+
+#include<stdio.h>
+int main(){
+
+    int i = 1;
+    while(i<=5){
+        printf("Radhe radhe : %d\n",i);
+        i++;
+    }
+
+    return 0;
+}
