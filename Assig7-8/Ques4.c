a412a29a4f54bb1fdc04a62c747529270b13a86d
connectoshivam <connecttoshivam1@gmail.com>
2026-02-01 10:58:36 +0530

    basics

---
+++ Assig7-8/Ques4.c
@@ -0,0 +1,15 @@
+// Q. write a program print 10 odd natural numbers in reverse ors=der.
+
+#include<stdio.h>
+int main(){
+
+    int i = 19;
+    while (i>=1)
+    {
+       printf("%d \n",i);
+       i -= 2;
+    }
+    
+
+    return 0;
+}
