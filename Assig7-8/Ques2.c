b8563546ff836024579849752104c389f44d97f3
connectoshivam <connecttoshivam1@gmail.com>
2026-02-01 10:20:18 +0530

    basics

---
+++ Assig7-8/Ques2.c
@@ -0,0 +1,15 @@
+// Q. write a program print the natural number in reverse order.
+
+#include<stdio.h>
+int main(){
+
+    int i = 10;
+    while (i>=1)
+    {
+        printf("%d \n",i);
+        i--;
+    }
+    
+
+    return 0;
+}
