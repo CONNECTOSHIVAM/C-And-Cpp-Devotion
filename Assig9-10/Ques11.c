43b5fda9b720633526c5b1441cdb447186393dff
connectoshivam <connecttoshivam1@gmail.com>
2026-02-03 23:06:15 +0530

    basics

---
+++ Assig9-10/Ques11.c
@@ -0,0 +1,26 @@
+// Q. find the fibonacci series up to n terms.
+
+#include<stdio.h>
+int main(){
+
+    int n,a=0,b=1;
+    printf("Enter the number:");
+    scanf("%d",&n);
+
+    if(n<=1)
+    {
+        printf("finbonacii number of %d: %d",n,n);
+    }
+    else{
+
+        int i = 2;
+        while (i<=n)
+        {
+            int temp = b;
+            b = b + a;
+            a = temp;
+            i++;
+        }
+        printf("fibonacci of %d number : %d",n,b);
+    }
+}
