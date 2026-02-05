f6fd285c5d665ff28c49e2bf368e3153ddcdaa73
connectoshivam <connecttoshivam1@gmail.com>
2026-02-05 22:43:39 +0530

    basics

---
+++ Assig9-10/Ques19.c
@@ -0,0 +1,36 @@
+// Q. write a program to check a given number is an armstrong number or not.    
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,sum=0;
+    printf("Enter the Number: ");
+    scanf("%d",&a);
+
+    int b = a;
+
+    int length = (int)log10(a) + 1;
+
+    while (a>0)
+    {
+        int z = a%10;
+        int x = 1;
+        for(int i=0;i<length;i++)
+        {
+          x = x*z;
+        }
+        sum += x;
+        a = a/10;
+    }
+    
+
+    if(b==sum)
+    {
+        printf("%d is a Armstrong Number.",b);
+    }else{
+        printf("%d is a not Armstrong Number.",b);
+    }
+
+    return 0;
+}
