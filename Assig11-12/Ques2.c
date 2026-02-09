a7a9501c9baf20687cc6c955437f82f5bd06bde1
connectoshivam <connecttoshivam1@gmail.com>
2026-02-09 23:00:33 +0530

    basics

--- Assig11-12/Ques2.c
+++ Assig11-12/Ques2.c
@@ -1,20 +1,24 @@
-// Q. Design a pattern
-// *****
-// ****
-// ***
-// **
-// *
+// Q.Design a pattern
+//         * 
+//       * * 
+//     * * * 
+//   * * * * 
+// * * * * * 
 
 #include <stdio.h>
 int main()
 {
     int a = 5;
 
-    for (int i = a; i >= 1; i--)
+    for(int i=1;i<=a;i++)
     {
-        for (int j = 1; j <= i; j++)
+        for(int j=1;j<=a-i;j++)
         {
-            printf("* ");
+            printf(" ");
+        }
+        for(int k=1;k<=i;k++)
+        {
+            printf("*");
         }
         printf("\n");
     }
