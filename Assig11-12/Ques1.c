83326034459ccb757a08b189b921b9d558954301
connectoshivam <connecttoshivam1@gmail.com>
2026-02-09 22:44:08 +0530

    basics

--- Assig11-12/Ques1.c
+++ Assig11-12/Ques1.c
@@ -1,18 +1,16 @@
-// Q. Design a pattern
+// Q.Design a pattern
 // *
-// **
-// ***
-// ****
-// *****
+// * *
+// * * * 
+// * * * * 
+// * * * * * 
+
 
 #include<stdio.h>
-int main()
-{
-    int a=5;
-    printf("Enter the size: ");
-    scanf("%d",&a);
+int main(){
 
-    for(int i = 1; i<=a;i++)
+    int a = 5;
+    for(int i = 1;i<=a;i++)
     {
         for(int j = 1; j<=i;j++)
         {
