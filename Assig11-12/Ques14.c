777a2819bc96c757d59c48d5206e2479e45d6f59
connectoshivam <connecttoshivam1@gmail.com>
2026-02-15 15:07:21 +0530

    basics

---
+++ Assig11-12/Ques14.c
@@ -0,0 +1,31 @@
+// Q.Design the following pattern.
+//       1
+//     1 2 1
+//   1 2 3 2 1
+// 1 2 3 4 3 2 1
+
+#include <stdio.h>
+int main()
+{
+
+    int n = 4;
+    for (int i = 1; i <= n; i++)
+    {
+        
+        for (int k = 1; k <= n - i; k++)
+        {
+            printf("  ");
+        }
+        for (int j = 1; j <=i; j++)
+        {
+            printf("%d ", j);
+        }
+        for(int j=i-1;j>=1;j--)
+        {
+            printf("%d ",j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
