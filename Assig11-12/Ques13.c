9806355b78be6434bdd2dbbb1f43280ae832776b
connectoshivam <connecttoshivam1@gmail.com>
2026-02-15 14:27:57 +0530

    basics

---
+++ Assig11-12/Ques13.c
@@ -0,0 +1,25 @@
+// Q. Design the following pattern.
+//     *
+//    * *
+//   * * *
+//  * * * *
+
+#include<stdio.h>
+int main(){
+
+    int n = 4;
+    for(int i=1;i<=n;i++)
+    {
+        for(int k=1;k<=n-i;k++)
+        {
+            printf(" ");
+        }
+        for(int j=1;j<=i;j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
