a76296576ab05fd36ff7ac8732ef51f892691a30
connectoshivam <connecttoshivam1@gmail.com>
2026-02-15 18:05:58 +0530

    basics

--- Assig11-12/Ques17.c
+++ Assig11-12/Ques17.c
@@ -15,11 +15,17 @@ int main()
         {
             printf("  ");
         }
-        for (int j = 1; j <= i; j++)
+        int peak,t;
+        if(i<=1) peak=1;
+        else if(i<=2) peak,t=1,2;
+        else if(i<=3) peak,t=2,2;
+        else if(i<=4) peak,t=2,3;
+        else peak=3;
+        for (int j = 1; j <= peak; j++)
         {
             printf("%d   ", j);
         }
-        for (int j = i - 1; j >= 1; j--)
+        for (int j = t - 1; j >= 1; j--)
         {
             printf("%d   ", j);
         }
