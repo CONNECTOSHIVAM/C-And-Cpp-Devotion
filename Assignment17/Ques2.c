461509095aeb3629bdbe8e2f9b37c90eb596e15f
connectoshivam <connecttoshivam1@gmail.com>
2026-05-09 09:40:39 +0530

    this all question are based on recursion analogy.

--- Assignment17/Ques2.c
+++ Assignment17/Ques2.c
@@ -1,1 +1,22 @@
-// Q2. 
+// Q2. write a function to calculate sum of first N odd natural numbers.
+#include<stdio.h>
+int sumOfOdd(int a);
+int main()
+{
+    int num;
+    printf("Enter the number: ");
+    scanf("%d",&num);
+
+    int result = sumOfOdd(num);
+    printf("Sum of odd first N number: %d",result);
+    return 0;
+}
+int sumOfOdd(int a)
+{
+   if(a==0)
+   {
+    return 0;
+   }
+   int b = a*2-1;
+   return b + sumOfOdd(a-1);
+}
