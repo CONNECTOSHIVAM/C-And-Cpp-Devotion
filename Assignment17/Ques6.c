18a718008e4018c45a9ccf188b67b4a7d266b32b
connectoshivam <connecttoshivam1@gmail.com>
2026-05-09 10:28:45 +0530

    this all question are based on recursion analogy.

--- Assignment17/Ques6.c
+++ Assignment17/Ques6.c
@@ -1,1 +1,21 @@
-// Q6.
+// Q6.write a recursive function to calculate the factorail of the given number.
+#include<stdio.h>
+int factorial(int a);
+int main()
+{
+    int num;
+    printf("Enter the number: ");
+    scanf("%d",&num);
+
+    int result = factorial(num);
+    printf("Factorial of %d : %d",num,result);
+    return 0;
+}
+int factorial(int a){
+
+    if(a==0)
+    {
+        return 1;
+    }
+    return a * factorial(a-1);
+}
