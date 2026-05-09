c83e6a8c53d69cbdf9c87634d48b9d5b89ecff6e
connectoshivam <connecttoshivam1@gmail.com>
2026-05-09 10:04:52 +0530

    this all question are based on recursion analogy.

--- Assignment17/Ques4.c
+++ Assignment17/Ques4.c
@@ -1,1 +1,21 @@
-// Q4. 
+// Q4. write a recursive function calculate sum of square of first n natural numbers.
+#include<stdio.h>
+int sumOfNSquare(int a);
+int main()
+{
+    int num;
+    printf("Enter the number: ");
+    scanf("%d",&num);
+
+    int result = sumOfNSquare(num);
+    printf("Sum of first %d square: %d",num,result);
+    return 0;
+}
+int sumOfNSquare(int a)
+{
+    if(a == 0)
+    {
+        return 0;
+    }
+    return (a*a)+sumOfNSquare(a-1);
+}
