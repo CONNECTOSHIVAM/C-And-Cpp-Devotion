6466f31c31d239b258a98b322678211ed311ca98
connectoshivam <connecttoshivam1@gmail.com>
2026-05-09 10:13:39 +0530

    this all question are based on recursion analogy.

--- Assignment17/Ques5.c
+++ Assignment17/Ques5.c
@@ -1,1 +1,21 @@
-// Q5. 
+// Q5. write a recursive function to sum of digits of a given numbers.
+#include<stdio.h>
+int sumOfDigits(int a);
+int main()
+{
+    int num;
+    printf("Enter the number: ");
+    scanf("%d",&num);
+
+    int result = sumOfDigits(num);
+    printf("sum of %d digits: %d",num,result);
+    return 0;
+}
+int sumOfDigits(int a)
+{
+    if(a==0)
+    {
+        return 0;
+    }
+    return a%10+sumOfDigits(a/10);
+}
