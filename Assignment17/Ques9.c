365eec1158501acf7a7a8129789f5d4619dc95a4
connectoshivam <connecttoshivam1@gmail.com>
2026-05-09 12:45:16 +0530

    this all question are based on recursion analogy.

--- Assignment17/Ques9.c
+++ Assignment17/Ques9.c
@@ -1,1 +1,22 @@
-// Q9.
+// Q9.write a program in c to count the digits of given number using recursion.
+
+#include<stdio.h>
+int count(int a);
+int main()
+{
+    int num;
+    printf("Enter the number: ");
+    scanf("%d",&num);
+
+    int result = count(num);
+    printf("Length of %d is : %d",num,result);
+    return 0;
+}
+int count(int a)
+{
+    if(a==0)
+    {
+        return 0;
+    }
+    return 1 +  count(a/10);
+}
