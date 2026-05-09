6916da6c6e5ba0f1c48c3edd83d3e20a5796b26c
connectoshivam <connecttoshivam1@gmail.com>
2026-05-09 13:09:49 +0530

    this all question are based on recursion analogy.

--- Assignment17/Ques10.c
+++ Assignment17/Ques10.c
@@ -1,1 +1,25 @@
-// Q10.
+// Q10.write a program in c to calculate the power of any number using recursion.
+#include<stdio.h>
+int power(int a, int b);
+int main()
+{
+    int num,pow;
+    printf("Enter the number: ");
+    scanf("%d",&num);
+    printf("Enter the power term: ");
+    scanf("%d",&pow);
+
+
+    int result = power(num,pow);
+    printf("%d^%d value is: %d",num,pow,result);
+    return 0;
+}
+int power(int a,int b)
+{
+    if(b==0)
+    {
+        return 1;
+    }
+    return a * power(a,b-1);
+
+}
