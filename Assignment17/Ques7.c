273958603ad2e2132cf1a8050e523a15f6b53e4e
connectoshivam <connecttoshivam1@gmail.com>
2026-05-09 11:25:29 +0530

    this all question are based on recursion analogy.

--- Assignment17/Ques7.c
+++ Assignment17/Ques7.c
@@ -1,1 +1,22 @@
-// Q7.
+// Q7.write a recursive function to calculate the HCF of 2 numbers.
+#include <stdio.h>
+int hcf(int x, int y);
+int main()
+{
+    int a, b;
+    printf("Enter the 2 number: ");
+    scanf("%d%d", &a, &b);
+
+    int result = hcf(a, b);
+    printf("HCF(%d,%d) is: %d ", a, b, result);
+    return 0;
+}
+
+int hcf(int x, int y)
+{
+    if(y==0)
+    {
+        return x;
+    };
+    return hcf(y,x%y);
+}
--- Assignment17/Ques8.c
+++ Assignment17/Ques8.c
@@ -1,1 +1,25 @@
 // Q8.
+// Q7.write a recursive function to calculate the HCF of 2 numbers.
+#include <stdio.h>
+int hcf(int x, int y);
+int main()
+{
+    int a, b;
+    printf("Enter the 2 number: ");
+    scanf("%d%d", &a, &b);
+
+    int result = hcf(a, b);
+    printf("HCF(%d,%d) is: %d ", a, b, result);
+    return 0;
+}
+
+int hcf(int x, int y)
+{
+    for (int i = x > y ? y : x; i >= 1; i--)
+    {
+        if (i % x == 0 && i % y == 0)
+        {
+            return i;
+        }
+    }
+}
