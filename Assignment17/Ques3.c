798527ea22134bbb69bc951d9e4cc0cf69a3dc84
connectoshivam <connecttoshivam1@gmail.com>
2026-05-09 09:46:55 +0530

    this all question are based on recursion analogy.

--- Assignment17/Ques3.c
+++ Assignment17/Ques3.c
@@ -1,1 +1,21 @@
-// Q3. 
+// Q3. write a recursive function to calulate sum of first N even natural numbers.
+#include<stdio.h>
+int sumOfEvenN(int a);
+int main()
+{
+    int num;
+    printf("Enter the number: ");
+    scanf("%d",&num);
+
+    int result = sumOfEvenN(num);
+    printf("Sum of first first %d even number: %d ",num,result);
+    return 0;
+}
+int sumOfEvenN(int a)
+{
+    if(a==0)
+    {
+        return 0;
+    }
+    return (a*2)+sumOfEvenN(a - 1);
+}
