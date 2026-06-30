b192a2f734500a48ff44ef18f24491097cec1ff7
connectoshivam <connecttoshivam1@gmail.com>
2026-06-30 21:54:54 +0530

    updated the files

--- Assignment19/Ques4.c
+++ Assignment19/Ques4.c
@@ -41,7 +41,7 @@ void rotateArray(int arr[],int size,int pos,int direc)
         while (pos)
         {
             temp = arr[size-1];
-            for (int i = size-2; i >=  0; i--)
+            for (int i = size-2; i >= 0; i--)
             {
                 arr[i+1] = arr[i];
             }
